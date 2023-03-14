20024fb319d67b7a2c104abaced217afe5c4d928
Vinícius Raniery <124690508+V1niciusR@users.noreply.github.com>
2023-03-14 11:00:11 -0300

    ...

---
+++ js/typed.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+     var typed = new Typed(".typing", {
+         strings:["Páginas Web", "Jogos", "Projetos", "Programas"],
+         typeSpeed:100,
+         backSpeed:60,
+         loop:true
+     });
+     var typed = new Typed(".typing-2", {
+        strings:["Fábrica de Software!"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop:true
+    });
+});
+
+AOS.init({
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 900, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+  
+  });
