0a61d885ce9e12375c59ac4270e7a6594eb86346
Vinícius Raniery <124690508+V1niciusR@users.noreply.github.com>
2023-03-17 19:46:55 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,40 @@
+/*======= Função principal =======*/
 function calcularCampo() {
 
     var carga = parseFloat(document.getElementById("carga").value.replace(',', '.'));
     var distancia = parseFloat(document.getElementById("distancia").value.replace(',', '.'));
+    var constante = 9.000000000e9;
+
+    /*
     var constante = 8.987551787e9;
+    */
+
     var campo = constante * carga / Math.pow(distancia, 2);
 
+    document.getElementById("resultado").innerHTML = "Campo elétrico gerado: " + campo.toFixed(7) + " N/C";
+
+    /*
     document.getElementById("resultado").innerHTML = "Campo elétrico gerado: " + campo.toExponential(2) + " N/C";
+    */
 }
+/*======= /Função principal =======*/
+
+
+
+/*======= Código pedindo o valor da constante =======*
+
+function calcularCampo() {
+
+    var carga = parseFloat(document.getElementById("carga").value.replace(',', '.'));
+    var distancia = parseFloat(document.getElementById("distancia").value.replace(',', '.'));
+    var constante = parseFloat(document.getElementById("constante").value.replace(',', '.'));
+
+
+    const k = parseFloat(document.getElementById('constante').value);
+    const q = parseFloat(document.getElementById('carga').value);
+    const d = parseFloat(document.getElementById('distancia').value);
+    const campo = k * q / (d * d);
+    document.getElementById('resultado').innerHTML = `O campo elétrico gerado pela carga é de ${campo.toFixed(2)} N/C.`;
+  }
+
+  /*======= /Código pedindo o valor da constante =======*/
